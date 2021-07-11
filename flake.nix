87908f0f61d28fcbd33c47755ec6b881222c5b0a
Gytis Ivaskevicius <me@gytis.io>
2021-07-11 21:35:03 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -24,12 +24,22 @@
           {
             name = "fetch-kernel-configs";
             command = ''
-              mkdir -p sources
+              mkdir -p $DEVSHELL_ROOT/sources
               curl https://src.fedoraproject.org/rpms/kernel/raw/main/f/kernel-x86_64-rhel.config -o sources/rhel
               curl https://src.fedoraproject.org/rpms/kernel/raw/main/f/kernel-x86_64-fedora.config -o sources/fedora
               curl https://raw.githubusercontent.com/clearlinux-pkgs/linux/master/config -o sources/clear
               curl https://raw.githubusercontent.com/clearlinux-pkgs/linux/master/config-fragment -o sources/clear-fragment
               curl https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/linux/trunk/config -o sources/arch
+            '';
+          }
+          {
+            name = "clean-sources";
+            command = ''
+              mkdir -p $DEVSHELL_ROOT/sources-clean
+
+              for f in $(find $DEVSHELL_ROOT/sources -type f); do
+                 grep '^[^#]' $f | sort > $DEVSHELL_ROOT/sources-clean/$(basename $f)
+              done
             '';
           }
         ];
